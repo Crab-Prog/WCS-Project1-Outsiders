0fef8e8f0f11677ef4e5678c4fcfcde69f71fc96
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-03-18 14:58:59 +0100

    modifications page Tour

---
+++ assets/javascript/tour.js
@@ -0,0 +1,5 @@
+const button = document.querySelectorAll('.buy_btn');
+button.forEach(item => item.addEventListener('click', () => {
+  window.open(url,"404.html");
+  console.log("bouton cliqué");
+}));
