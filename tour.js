fac1f4adc730a8acecfa4be5db6b6c2964f72dda
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-03-18 15:22:16 +0100

    test commit js

--- tour.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelectorAll('.buy_btn');
-button.forEach(item => item.addEventListener('click', () => {
-  window.open(url,"404.html")
-  console.log("bouton cliqué")
-}));
