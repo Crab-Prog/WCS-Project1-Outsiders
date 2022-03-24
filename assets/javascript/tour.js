8ab55ef567bb9fa2f926612be0bf15c761db27eb
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-03-24 08:32:02 +0100

    ajout page 404 à tour.js pour action bouton

--- assets/javascript/tour.js
+++ assets/javascript/tour.js
@@ -1,6 +1,6 @@
 const button = document.querySelectorAll('.buy_btn');
 button.forEach(item => item.addEventListener('click', () => {
-  window.open(url,"404.html");
+  window.location.href="404.html";
   console.log("bouton cliqué");
 }));
 // test commit
